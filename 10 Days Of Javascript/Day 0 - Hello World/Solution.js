121ff58f4ea3fe2f564bf722616de6e83ecf3c97
yasemincidem <yasemincidem@gmail.com>
2017-10-24 11:00:06 +0300

    Hello World Solution is added

---
+++ 10 Days Of Javascript/Day 0 - Hello World/Solution.js
@@ -0,0 +1,7 @@
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+}
+greeting("Welcome to 10 Days of JavaScript!");
